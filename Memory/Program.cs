4f8dd60555ec4a7c2923be1a5988b633af1645cc
JanickSpielmann <janick.spielmann@messageme.ch>
2022-11-14 13:29:14 +0100

    something

--- Memory/Program.cs
+++ Memory/Program.cs
@@ -1,41 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Media;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Memory
 {
-    
     internal static class Program
     {
-        public static String path = System.IO.Path.GetFullPath(@"..\..\");
-        static void Main(string[] args)
+        public static string path = Path.GetFullPath(@"..\..\");
+
+        private static void Main(string[] args)
         {
-            
-            
             //Settings
-            int[] size = new int[] { 2,6};
-            bool sound = false;
-            
+            int[] size = { 2, 6 };
+            var sound = true;
+
             //Variables
             size[0] = size[0] + 1; // adding size for the coordinates
             size[1] = size[1] + 1; // adding size for the coordinates
 
 
             //Arrays
-            int[] a = new int[] { 0, 0 }; // 1 coordinate (x,y)
-            int[] b = new int[] { 0, 0 }; // 2.coordinate (x,y)
-            char[] symbols = new char[]
+            int[] a = { 0, 0 }; // 1 coordinate (x,y)
+            int[] b = { 0, 0 }; // 2.coordinate (x,y)
+            char[] symbols =
             {
                 '☺', '☻', '♦', '♠', '♥', '♣', '♫', '☼', '©', '&', '▲', '►', '▼', '◄', '#', '§', '$', '£', '✿', '✌', '☁',
                 '❈', '☂', '☎', '▣', '☃'
             };
-            char[,] blancField = new char[size[0], size[1]];
-            char[,] cardField = new char[size[0], size[1]];
-            Random random = new Random();
+            var blancField = new char[size[0], size[1]];
+            var cardField = new char[size[0], size[1]];
+            var random = new Random();
 
 
             blancField = MakeCleanArray(size);
@@ -46,33 +40,7 @@ namespace Memory
             Console.WriteLine("-----------------------------");
             ShowField(blancField, size);
 
-            while (!CheckIfFinish(blancField, size))
-            {
-                blancField = playRound(blancField, cardField, size, sound);
-            }
-
-
-            /*
-            for (int i = 0; i < 2; i++)
-            {
-                ShowField(blancField, size);
-
-
-                a = InputCardLocation(size);
-                b = InputCardLocation(size);
-
-                if (cardField[a[0], a[1]] == cardField[b[0], b[1]])
-                {
-                    Console.WriteLine("Got one");
-                    blancField[a[0], a[1]] = cardField[a[0], a[1]];
-                    blancField[b[0], b[1]] = cardField[b[0], b[1]];
-                }
-                else
-                {
-                    Console.WriteLine("try again");
-                }
-            }
-            */
+            while (!CheckIfFinish(blancField, size)) blancField = playRound(blancField, cardField, size, sound);
 
 
             /////////////////!!!!!!You shall not pass!!!!!////////////////
@@ -81,7 +49,7 @@ namespace Memory
         }
 
 
-        static void ThisIsTheEnd(bool sound)
+        private static void ThisIsTheEnd(bool sound)
         {
             Jukebox(sound);
 
@@ -90,86 +58,63 @@ namespace Memory
 
         private static void Jukebox(bool sound)
         {
-
             //Songs
-            SoundPlayer Skyfall = new SoundPlayer(@path + "sounds\\Skyfall.wav");
-            SoundPlayer InTheEnd = new SoundPlayer(@path + "sounds\\InTheEnd.wav");
-            SoundPlayer GoodThingsComeToAnEnd = new SoundPlayer(@path + "sounds\\GoodThingsComeToAnEnd.wav");
+            var Skyfall = new SoundPlayer(path + "sounds\\Skyfall.wav");
+            var InTheEnd = new SoundPlayer(path + "sounds\\InTheEnd.wav");
+            var GoodThingsComeToAnEnd = new SoundPlayer(path + "sounds\\GoodThingsComeToAnEnd.wav");
 
 
-            Random Song = new Random();
+            var Song = new Random();
             switch (Song.Next(1, 4))
             {
                 case 1:
-                     if(sound){Skyfall.Play();}
+                    if (sound) Skyfall.Play();
                     Console.WriteLine(
                         "This is the end...\nHold your breath and count to ten.\nFeel the Earth move and then.\nHear my heart burst again.\nFor this is the end.\nI've drowned and dreamt this moment.\nSo overdue, I owe them.\nSwept away, I'm stolen.\n");
                     break;
                 case 2:
-                    if (sound)
-                    {
-                        InTheEnd.Play();
-                    }
+                    if (sound) InTheEnd.Play();
 
                     Console.WriteLine(
                         "I tried so hard and got so far\nBut in the end, it doesn't even matter\nI had to fall to lose it all\nBut in the end, it doesn't even matter");
                     break;
                 case 3:
-                if (sound)
-                {
-                    GoodThingsComeToAnEnd.Play();
-                }
+                    if (sound) GoodThingsComeToAnEnd.Play();
 
-                Console.WriteLine(
+                    Console.WriteLine(
                         "Flames to dust, lovers to friends\nWhy do all good things come to an end?\nFlames to dust, lovers to friends\nWhy do all good things come to an end?");
                     break;
             }
         }
 
         /////////////////////////////
-        static void ShowField(char[,] field, int[] size)
+        private static void ShowField(char[,] field, int[] size)
         {
-            for (int i = 0; i < size[0]; i++)
+            for (var i = 0; i < size[0]; i++)
             {
-                for (int j = 0; j < size[1] + 1; j++)
-                {
+                for (var j = 0; j < size[1] + 1; j++)
                     if (j == size[1])
-                    {
-                        Console.Write($"{((char)(i + 48)),4}");
-                    }
+                        Console.Write($"{(char)(i + 48),4}");
                     else
-                    {
                         Console.Write($"{field[i, j],4}");
-                    }
-                }
 
                 Console.WriteLine();
                 Console.WriteLine();
             }
         }
 
-        static char[,] MakeCleanArray(int[] size)
+        private static char[,] MakeCleanArray(int[] size)
         {
-            char[,] aArray = new char[size[0], size[1]];
-            for (int i = 0; i < size[0]; i++)
-            {
-                for (int j = 0; j < size[1]; j++)
-                {
-                    if (i == 0)
-                    {
-                        aArray[i, j] = ((char)(j + 48));
-                    }
+            var aArray = new char[size[0], size[1]];
+            for (var i = 0; i < size[0]; i++)
+            for (var j = 0; j < size[1]; j++)
+                if (i == 0)
+                    aArray[i, j] = (char)(j + 48);
 
-                    else if (j == 0)
-                    {
-                        aArray[i, j] = ((char)(i + 48));
-                    }
-                    else
-                    {
-                        aArray[i, j] = '■';
-                    }
-                }
-            }
+                else if (j == 0)
+                    aArray[i, j] = (char)(i + 48);
+                else
+                    aArray[i, j] = '■';
 
             return aArray;
         }
@@ -181,20 +126,18 @@ namespace Memory
             aArray = MakeCleanArray(size);
             var random = new Random();
             for (var i = 0; i < (size[0] - 1) * (size[1] - 1) / 2; i++)
+            for (var j = 0; j < 2; j++)
             {
-                for (var j = 0; j < 2; j++)
+                var placed = false;
+                while (!placed)
                 {
-                    var placed = false;
-                    while (!placed)
-                    {
-                        var a = random.Next(size[0] - 1); // x coordinate new object
-                        var b = random.Next(size[1] - 1); // y coordinate new object
+                    var a = random.Next(size[0] - 1); // x coordinate new object
+                    var b = random.Next(size[1] - 1); // y coordinate new object
 
-                        if (aArray[a + 1, b + 1] == '■')
-                        {
-                            aArray[a + 1, b + 1] = symbols[i];
-                            placed = true;
-                        }
+                    if (aArray[a + 1, b + 1] == '■')
+                    {
+                        aArray[a + 1, b + 1] = symbols[i];
+                        placed = true;
                     }
                 }
             }
@@ -203,101 +146,97 @@ namespace Memory
         }
 
 
-
-        static int[] InputCardLocation(char[,] blancField, int[] size)
+        private static int[] InputCardLocation(char[,] blancField, int[] size)
         {
-            int[] iArray;
+            var iArray = new int[2];
             bool number;
+            var error = false;
 
             do
             {
                 number = true;
-                char[] cArray = Console.ReadLine().ToCharArray();
-                
-                iArray = new int[cArray.Length];
-                Console.WriteLine();
-                iArray[0] = cArray[0] - 48;
-                iArray[1] = cArray[1] - 48;
-                if ((iArray[0] < 0) || (iArray[0] > size[0] - 1) || (iArray[1] < 0) || (iArray[1] > size[1] - 1))
+
+                try
                 {
-                    number = false;
-                    Console.WriteLine("This is not a number or it is outside the filed");
-                    Console.Write("Input new coordinates");
+                    var cArray = Console.ReadLine().ToCharArray();
+                    iArray = new int[cArray.Length];
+                    Console.WriteLine();
+                    iArray[0] = cArray[0] - 48;
+                    iArray[1] = cArray[1] - 48;
+                    if (iArray[0] < 0 || iArray[0] > size[0] - 1 || iArray[1] < 0 || iArray[1] > size[1] - 1)
+                    {
+                        number = false;
+                        Console.WriteLine("This is not a number or it is outside the field");
+                        Console.Write("Input new coordinates: ");
+                    }
+                    else if (blancField[iArray[0], iArray[1]] != '■')
+                    {
+                        number = false;
+                        Console.WriteLine("This card has been turned before");
+                        Console.Write("Input new coordinates: ");
+                    }
                 }
-                else if ((blancField[iArray[0],iArray[1]]) != '■')
+                catch (IndexOutOfRangeException)
                 {
+                    Console.Write("you have to input coordinates: ");
                     number = false;
-                    Console.WriteLine("This card has been turned before");
-                    Console.Write("Input new coordinates:");
                 }
-               
             } while (!number);
 
             return iArray;
         }
 
-        public static char[,] playRound(char[,] blancField, char[,] cardField, int[] size,bool sound)
+        public static char[,] playRound(char[,] blancField, char[,] cardField, int[] size, bool sound)
         {
-            int[] cardA = new int[] { 0, 0 };
-            int[] cardB = new int[] { 0, 0 };
-            
-            
+            int[] cardA = { 0, 0 };
+            int[] cardB = { 0, 0 };
+
+
             Console.Write("please enter Coordinates (11,23) for the first card: ");
-            cardA = InputCardLocation(blancField,size);
+            cardA = InputCardLocation(blancField, size);
             blancField[cardA[0], cardA[1]] = cardField[cardA[0], cardA[1]];
             ShowField(blancField, size);
             do
             {
-                if ((cardA[0] == cardB[0])&&(cardA[1] == cardB[1]))
-                {
-                    Console.WriteLine("This is the same Card, you have to have 2 cards, change ");
-                }
+                if (cardA[0] == cardB[0] && cardA[1] == cardB[1])
+                    Console.WriteLine("This is the same Card, you have to have 2 cards, change: ");
+
                 Console.Write("please enter Coordinates (11,23) for the second card: ");
                 cardB = InputCardLocation(blancField, size);
-            } while ((cardA[0] == cardB[0])&&(cardA[1] == cardB[1]));
+            } while (cardA[0] == cardB[0] && cardA[1] == cardB[1]);
+
             blancField[cardB[0], cardB[1]] = cardField[cardB[0], cardB[1]];
             ShowField(blancField, size);
-            Console.WriteLine("-----------");
-            if (!((cardField[cardA[0], cardA[1]]) == (cardField[cardB[0], cardB[1]])))
+            if (!(cardField[cardA[0], cardA[1]] == cardField[cardB[0], cardB[1]]))
             {
                 blancField[cardA[0], cardA[1]] = '■';
                 blancField[cardB[0], cardB[1]] = '■';
-                Console.WriteLine("Try again");
-                SoundPlayer Duck = new SoundPlayer(@path + "sounds\\WhatTheDuck.wav");
-                if (sound)
-                {
-                    Duck.Play();
-                }
+                var Duck = new SoundPlayer(path + "sounds\\WhatTheDuck.wav");
+                if (sound) Duck.Play();
+                Console.WriteLine("Try again?");
+                Console.ReadLine();
             }
             else
             {
-                SoundPlayer Yeah = new SoundPlayer(@path + "sounds\\Yeah.wav");
-                if (sound)
-                {
-                    Yeah.Play();
-                }
+                var Yeah = new SoundPlayer(path + "sounds\\Yeah.wav");
+                if (sound) Yeah.Play();
 
                 Console.WriteLine("You got one! ENTER the next round");
                 Console.ReadLine();
-
             }
+
             ShowField(blancField, size);
-            
+
             return blancField;
         }
+
         public static bool CheckIfFinish(char[,] blancField, int[] size)
         {
-            bool finish = true;
-            for (int i = 0; i < size[0]; i++)
-            {
-                for (int j = 0; j < size[1]; j++)
-                {
-                    if (blancField[i, j] == '■')
-                    {
-                        finish = false;
-                    }
-                }
-            }
+            var finish = true;
+            for (var i = 0; i < size[0]; i++)
+            for (var j = 0; j < size[1]; j++)
+                if (blancField[i, j] == '■')
+                    finish = false;
 
             return finish;
         }
